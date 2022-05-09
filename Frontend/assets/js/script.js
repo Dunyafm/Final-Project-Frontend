5abfef71eaec21bee3d5a4d0a02028b659a99182
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-09 21:10:27 +0400

    Created Catergories

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -11,6 +11,22 @@
 
 })(jQuery);
 
+// CATERGORIES
+//filter products
+var $mediaElements = $(".cd-item");
+$(".filter_link").click(function(e) {
+  e.preventDefault();
+  var filterVal = $(this).data("filter");
+  if (filterVal === "all") {
+    $mediaElements.slideDown("slow");
+  } else {
+    $mediaElements
+      .hide("slow")
+      .filter("." + filterVal)
+      .slideDown("slow");
+  }
+});
+//filter products
 
 // $(document).ready(function () {
 
