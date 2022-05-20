8aab99f3133602b78443aa2bfddaf7f25cc0328b
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-21 03:21:31 +0400

    Brand logo add brands

--- Frontend/assets/js/home.js
+++ Frontend/assets/js/home.js
@@ -80,39 +80,6 @@ $(this).addClass("active");
 })();
 
 
-// BRANDS LOGO JS
-$(function() {
-    var $clientslider = $('#clientlogo');
-    var clients = $clientslider.children().length;
-    var clientwidth = (clients * 220); 
-    $clientslider.css('width', clientwidth);
-    var rotating = true;
-    var clientspeed = 1800;
-    var seeclients = setInterval(rotateClients, clientspeed);
-    $(document).on({
-      mouseenter: function() {
-        rotating = false;
-      },
-      mouseleave: function() {
-        rotating = true;
-      }
-    }, '#ourclients');
-    function rotateClients() {
-      if (rotating != false) {
-        var $first = $('#clientlogo li:first');
-        $first.animate({
-          'margin-left': '-220px'
-        }, 2000, function() {
-          $first.remove().css({
-            'margin-left': '0px'
-          });
-          $('#clientlogo li:last').after($first);
-        });
-      }
-    }
-  });
-
-
 // RESPONSIVE CARD
 $(function() {
   $('.material-card > .mc-btn-action').click(function () {
