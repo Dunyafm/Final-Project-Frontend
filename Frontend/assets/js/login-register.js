ad1c3dee280a55341e74777c3d3e9b79db7c8571
Dunya Mammadli <dunyamamadli@gmail.com>
2022-05-21 18:24:01 +0400

    Catergories changed

--- Frontend/assets/js/login-register.js
+++ Frontend/assets/js/login-register.js
@@ -1,3 +1,22 @@
+// SEARCH
+
+function toggleShow () {
+    var el = document.getElementById("box");
+    el.classList.toggle("show");
+  }
+(function ($) {
+
+  $(document).ready(function () {
+    // Pause hero carousel
+    $('.hero-carousel').carousel('pause');
+  });
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+})(jQuery);
+
 $(document).ready(function(){
 
 //--------- change color value of the form text/password inputs -----
